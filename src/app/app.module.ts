37e7403b476ebaeb993e0fe28e37704c9998817b
Ana Lydia Moreira <analydiamoreira965@gmail.com>
2023-02-22 06:36:24 -0300

    Adding CSS, MaterialLibrary, BrowserAnimation,Flex-Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
-
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
 
   ],
   providers: [],
